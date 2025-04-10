94532e18dd12010e3211ea52835c742a6188af91
varuka999 <vjkpokemon@yahoo.com>
2025-04-10 10:42:43 -0700

    Week1 p1

---
+++ VGP133_Week1/VGP133_Week1/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+#region Strings
+//string name1 = "Charles";
+//string name2 = "Bobby";
+//string name3 = "   Spaces   ";
+
+//Console.WriteLine("Hello, World!");
+//Console.Write("No new line. ");
+//Console.WriteLine("Hello, World Again!");
+
+//Console.WriteLine($"Hello {name1} and {name2}!");
+
+//Console.WriteLine(@"C:\Documents ""files"".");
+
+//Console.WriteLine($"Hello {name3}.");
+//name3 = name3.Trim();
+//Console.WriteLine($"Hello {name3}.");
+//name3 = name3.Replace("Spaces", "Trimed");
+//Console.WriteLine($"Hello {name3}.");
+#endregion
+
+#region Numbers and Math
+//int a = 25;
+//int b = 10;
+//float c = 5.5f;
+//int sum = a + b;
+//int diff = a - b;
+//int prod = a * b;
+//float div = (float)a / (float)b;
+//int mod = a % b;
+
+//Console.WriteLine($"Math operations of {a} and {b}\n");
+//Console.WriteLine($"Sum: {sum}");
+//Console.WriteLine($"Difference: {diff}");
+//Console.WriteLine($"Product: {prod}");
+//Console.WriteLine($"Division: {div}");
+//Console.WriteLine($"Modulus: {mod}");
+#endregion
+
+#region Exercise 1
+string food = "Borger";
+int ing1 = 2;
+float ing2 = 2.5f;
+decimal ing3 = 3.3m;
+
+Console.WriteLine($"Step by step guide on how to cook {food}\n");
+Console.WriteLine($"Step 1: Prepare {ing1} loafs of bread bun.");
+Console.WriteLine($"Step 2: Cook {ing2} pounds of pork and place it onto 1 bread bun.");
+Console.WriteLine($"Step 3: Add {ing3} kg of melted cheese.");
+Console.WriteLine($"Step 4: Add the second bread bun on top.");
+Console.WriteLine($"Step 5: Eat");
+#endregion
+
+#region Reading Console
+string input1;
+Console.WriteLine("Enter your name:");
+#endregion
+
